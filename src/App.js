7cd96f342ad7fdf5bd40c9a10f51877c9d9f5d28
Your Name <you@example.com>
2023-02-21 23:07:34 +0530

    Completed projects section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import { NavBar } from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Banner } from './components/Banner';
 import { Skills } from './components/Skills';
+import { Projects } from './components/Projects';
 
 
 function App() {
@@ -12,6 +12,7 @@ function App() {
       <NavBar />
       <Banner/>
       <Skills/>
+      <Projects/>
     </div>
   );
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,25 +2,12 @@ import { Container, Row, Col, Nav, Tab } from "react-bootstrap";
 import projImg1 from '../assets/img/project-img1.png';
 import projImg2 from '../assets/img/project-img2.png';
 import projImg3 from '../assets/img/project-img3.png';
+import colorSharp2 from '../assets/img/color-sharp2.png';
+import { ProjectCard } from "./ProjectCard";
 
 export const Projects = () => {
 
     const projects = [
-        {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: projImg1,
-        },
-        {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: projImg2,
-        },
-        {
-            title: "Business Startup",
-            description: "Design & Development",
-            imgUrl: projImg3,
-        },
         {
             title: "Business Startup",
             description: "Design & Development",
@@ -46,7 +33,7 @@ export const Projects = () => {
                         <h2>Projects</h2>
                         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic na</p>
                         <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                            <Nav variant="pills" defaultActiveKey="/home">
+                            <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                                 <Nav.Item>
                                     <Nav.Link eventKey="first">Tab 1</Nav.Link>
                                 </Nav.Item>
@@ -63,7 +50,7 @@ export const Projects = () => {
                                         {
                                             projects.map((project, index) => {
                                                 return (
-                                                    <p>{project.title}</p>
+                                                    <ProjectCard key={index}{...project} />
                                                 )
                                             })
                                         }
@@ -75,12 +62,12 @@ export const Projects = () => {
                                 <Tab.Pane eventKey="third">
                                     Lorem Ipsum
                                 </Tab.Pane>
-
                             </Tab.Content>
                         </Tab.Container>
                     </Col>
                 </Row>
             </Container>
+            <img className="background-image-right" src={colorSharp2} />
         </section>
     )
 }
