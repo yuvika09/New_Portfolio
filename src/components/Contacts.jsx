8de91866d5b52a9483a4153b5fdc0062e95c037c
Your Name <you@example.com>
2023-07-20 23:25:17 +0530

    final commit

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -26,8 +26,6 @@ export const Contacts = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         setButtonText("Sending...");
-
-        
         let response = await fetch("http://localhost:3000/contact", {
             method: "POST",
             headers: {
@@ -38,7 +36,7 @@ export const Contacts = () => {
         setButtonText("Send");
         let result = await response.json();
         setFormDetails(formInitialDetails);
-        if (result.code == 200) {
+        if (result.code === 200) {
             setStatus({ succes: true, message: 'Message sent successfully' });
         } else {
             setStatus({ succes: false, message: 'Something went wrong, please try again later.' });
@@ -67,7 +65,7 @@ export const Contacts = () => {
                                                 <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
                                             </Col>
                                             <Col size={12} sm={6} className="px-1">
-                                                <input type="text" value={formDetails.lastName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)} />
+                                                <input type="text" value={formDetails.lasttName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)} />
                                             </Col>
                                             <Col size={12} sm={6} className="px-1">
                                                 <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
